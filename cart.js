698b850d606ad1cb8f4b964bc37ccb8b6379c011
ritul-ui <ritulanand1996@gmail.com>
2025-01-25 13:58:43 +0530

    cart page created using dom price and quantity and alert added

---
+++ cart.js
@@ -0,0 +1,18 @@
+const cartProducts = [
+
+    { id: 1, name: 'Laptop', price: 1200.00, quantity: 1, image: "https://placehold.co/400x300/f1c1b9/white?text=Premium+Leather+Bag" },
+   
+    { id: 2, name: 'Headphones', price: 150.00, quantity: 1, image: "https://placehold.co/400x300/f1c1b9/white?text=Premium+Leather+Bag" },
+   
+    { id: 3, name: 'Mouse', price: 25.00, quantity: 1, image: "https://placehold.co/400x300/f1c1b9/white?text=Premium+Leather+Bag" },
+   
+    { id: 4, name: 'Airpods', price: 120.00, quantity: 1, image: "https://placehold.co/400x300/f1c1b9/white?text=Premium+Leather+Bag" },
+    { id: 5, name: 'Cable', price: 10.00, quantity: 1, image: "https://placehold.co/400x300/f1c1b9/white?text=Premium+Leather+Bag" },
+
+ { id: 6, name: 'Keyboard', price: 50.00, quantity: 1, image: "https://placehold.co/400x300/f1c1b9/white?text=Premium+Leather+Bag" },
+
+ { id: 7, name: 'Cable', price: 10.00, quantity: 1, image: "https://placehold.co/400x300/f1c1b9/white?text=Premium+Leather+Bag" },
+
+ { id: 8, name: 'Keyboard', price: 50.00, quantity: 1, image: "https://placehold.co/400x300/f1c1b9/white?text=Premium+Leather+Bag" }
+
+ ]
